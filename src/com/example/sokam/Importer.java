71beeaea279db94ed1e929881ee3feeb5bc6df2e
sokampdx <sokam@pdx.edu>
2015-02-04 14:37:21 -0800

    added ignore and setup initial project

---
+++ src/com/example/sokam/Importer.java
@@ -0,0 +1,9 @@
+package com.example.sokam;
+
+public class Importer {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
---
+++ src/com/example/sokam/test/ImporterTest.groovy
@@ -0,0 +1,12 @@
+package com.example.sokam.test
+
+import com.example.sokam.Importer
+
+/**
+ * Created by sokam on 2/4/15.
+ */
+class ImporterTest extends GroovyTestCase {
+    void testMain() {
+        Importer importer = new Importer();
+    }
+}
