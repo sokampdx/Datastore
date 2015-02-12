17a89a390e674de3c0e075cd48a67c680e1b4274
sokampdx <sokam@pdx.edu>
2015-02-11 18:17:19 -0800

    added DoubleRecord which MoneyRecord extended from.

---
+++ src/dataStore/DoubleRecord.java
@@ -0,0 +1,58 @@
+package dataStore;
+
+/**
+ * Created by sokam on 2/7/15.
+ */
+public class DoubleRecord extends Record {
+
+  public static final String DATA_NOT_IN_CORRECT_FORMAT = "Data must be in number format. e.g. 3.25";
+  public static final String COMPARED_TO_DIFFERENT_OBJECT = "Must compare to same data Type";
+  public static final String REGEX = "(0|([1-9]\\d*))(\\.[0-9]*)?";
+
+  public DoubleRecord() {
+    super();
+  }
+
+  public DoubleRecord(String data) {
+    super();
+    setData(data);
+  }
+
+  public void setData(String data) {
+    if (data.matches(REGEX)) {
+      super.setData(data);
+    } else {
+      System.out.println(data);
+      throw new IllegalArgumentException(DATA_NOT_IN_CORRECT_FORMAT);
+    }
+  }
+
+  @Override
+  public boolean isSummable() {
+    return true;
+  }
+
+  public boolean isValid () {
+    return DoubleRecord.isValid(getData());
+  }
+
+  public static boolean isValid (String data) {
+    return data.matches(REGEX);
+  }
+
+  @Override
+  public int compareTo(Record record) throws IllegalArgumentException {
+    if (record instanceof DoubleRecord) {
+      if (this.isValid() && record.isValid()) {
+        Double fst = Double.parseDouble(this.getData());
+        Double snd = Double.parseDouble(record.getData());
+        return fst.compareTo(snd);
+      } else {
+        throw new IllegalArgumentException(DATA_NOT_IN_CORRECT_FORMAT);
+      }
+
+    } else {
+      throw new IllegalArgumentException(COMPARED_TO_DIFFERENT_OBJECT);
+    }
+  }
+}
--- src/dataStore/MoneyRecord.java
+++ src/dataStore/MoneyRecord.java
@@ -3,13 +3,11 @@ package dataStore;
 /**
  * Created by sokam on 2/7/15.
  */
-public class MoneyRecord extends Record {
+public class MoneyRecord extends DoubleRecord {
 
   public static final String DATA_NOT_IN_CORRECT_FORMAT = "Data must be in Dollar and Cents. e.g. 3.25";
   public static final String COMPARED_TO_DIFFERENT_OBJECT = "Must compare to same data Type";
   public static final String REGEX = "\\d+\\.\\d{2}";
-
-  // TODO: MoneyRecord extends DoubleRecord extends Record. Create string dollar format method
 
   public MoneyRecord(String data) {
     super();
@@ -24,12 +22,6 @@ public class MoneyRecord extends Record {
     }
   }
 
-
-  @Override
-  public boolean isSummable() {
-    return true;
-  }
-
   public boolean isValid () {
     return MoneyRecord.isValid(getData());
   }
@@ -38,19 +30,4 @@ public class MoneyRecord extends Record {
     return data.matches(REGEX);
   }
 
-  @Override
-  public int compareTo(Record record) throws IllegalArgumentException {
-    if (record instanceof MoneyRecord) {
-      if (this.isValid() && record.isValid()) {
-        Double fst = Double.parseDouble(this.getData());
-        Double snd = Double.parseDouble(record.getData());
-        return fst.compareTo(snd);
-      } else {
-        throw new IllegalArgumentException(DATA_NOT_IN_CORRECT_FORMAT);
-      }
-
-    } else {
-      throw new IllegalArgumentException(COMPARED_TO_DIFFERENT_OBJECT);
-    }
-  }
 }
